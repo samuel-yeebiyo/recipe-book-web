4965b34b18aab1a5f566d9aec69fbc57b0a8e4d8
Tawanda <ejmunongo@gmail.com>
2022-09-05 17:17:29 +0200

    update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,6 +10,7 @@ const Login = () => {
       <div className="h-3/5 w-3/5 border-2 p-4 border-gray-500 rounded-md">
         <p className="font-semibold text-xl">Pick a Login Option</p>
         <LoginButton value="http://localhost:8008/auth/google" />
+        <LoginButton value="http://localhost:8008/auth/google" />
       </div>
     </div>
   );
